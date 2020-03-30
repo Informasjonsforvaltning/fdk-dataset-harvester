f17697d9c7fdf4d9f04ace41d96552b43dcdef04
NilsOveTen <nils.ove.tendenes@acando.no>
2020-03-30 14:16:13 +0200

    feat: parse dataset rdf response

--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/harvester/DatasetHarvester.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/harvester/DatasetHarvester.kt
@@ -4,6 +4,7 @@ import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.adapter.DatasetAd
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.dto.HarvestDataSource
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.JenaType
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.jenaTypeFromAcceptHeader
+import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.parseRDFResponse
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 import java.util.*
@@ -20,7 +21,8 @@ class DatasetHarvester(private val adapter: DatasetAdapter) {
         if (jenaWriterType == null || jenaWriterType == JenaType.NOT_ACCEPTABLE) {
             LOGGER.error("Not able to harvest from ${source.url}, header ${source.acceptHeaderValue} is not acceptable ")
         } else {
-            LOGGER.info(adapter.getDataServiceCatalog(source))
+            adapter.getDataServiceCatalog(source)
+                ?.let { parseRDFResponse(it, jenaWriterType) }
         }
     }
 }
