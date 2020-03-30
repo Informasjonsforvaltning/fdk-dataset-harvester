cae17c3a28712c8ff888bcb770444a762923aaea
NilsOveTen <nils.ove.tendenes@acando.no>
2020-03-30 12:25:05 +0200

    feat: initiate harvest on startup

--- src/main/java/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/Application.java
+++ src/main/java/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/Application.java
@@ -1,5 +1,6 @@
 package no.digdir.informasjonsforvaltning.fdk_dataset_harvester;
 
+import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.configuration.ApplicationProperties;
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.configuration.FusekiProperties;
 import org.apache.jena.riot.RIOT;
 import org.springframework.boot.SpringApplication;
@@ -7,7 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({ FusekiProperties.class })
+@EnableConfigurationProperties({ ApplicationProperties.class, FusekiProperties.class })
 public class Application {
 
     public static void main(String[] args) {
