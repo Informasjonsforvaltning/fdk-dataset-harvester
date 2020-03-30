ebc651f8113ca7b6ab29d402c91261fe1f3ba0ae
NilsOveTen <nils.ove.tendenes@acando.no>
2020-03-30 08:30:22 +0200

    feat: add datasets to catalog responses

--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/service/CatalogService.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/service/CatalogService.kt
@@ -1,14 +1,18 @@
 package no.digdir.informasjonsforvaltning.fdk_dataset_harvester.service
 
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.fuseki.CatalogFuseki
+import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.fuseki.DatasetFuseki
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.JenaType
+import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.createIdFromUri
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.createRDFResponse
+import org.apache.jena.rdf.model.Model
+import org.apache.jena.rdf.model.ModelFactory
 import org.apache.jena.vocabulary.DCAT
 import org.apache.jena.vocabulary.RDF
 import org.springframework.stereotype.Service
 
 @Service
-class CatalogService(private val catalogFuseki: CatalogFuseki) {
+class CatalogService(private val catalogFuseki: CatalogFuseki, private val datasetFuseki: DatasetFuseki) {
 
     fun countDatasetCatalogs(): Int =
         catalogFuseki.fetchCompleteModel()
@@ -19,11 +23,36 @@ class CatalogService(private val catalogFuseki: CatalogFuseki) {
     fun getAllDatasetCatalogs(returnType: JenaType): String =
         catalogFuseki
             .fetchCompleteModel()
+            .addDatasetModels()
             .createRDFResponse(returnType)
 
     fun getDatasetCatalog(id: String, returnType: JenaType): String? =
         catalogFuseki
             .fetchByGraphName(id)
+            ?.addDatasetModels()
             ?.createRDFResponse(returnType)
+
+    private fun Model.addDatasetModels(): Model {
+
+        var unionModel = ModelFactory.createDefaultModel().union(this)
+
+        val datasetIdList = mutableListOf<String>()
+
+        listResourcesWithProperty(RDF.type, DCAT.Catalog)
+            .toList()
+            .forEach { catalog ->
+                catalog.listProperties(DCAT.dataset)
+                    .toList()
+                    .forEach { dataService ->
+                        datasetIdList.add(createIdFromUri(dataService.resource.uri))
+                    } }
+
+        datasetIdList
+            .toList()
+            .mapNotNull { id -> datasetFuseki.fetchByGraphName(id) }
+            .forEach { unionModel = unionModel.union(it) }
+
+        return unionModel
+    }
 
 }
