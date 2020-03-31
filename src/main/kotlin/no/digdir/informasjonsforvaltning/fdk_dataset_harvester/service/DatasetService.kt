10b3072a0b7da07ac3fbb0c021fad934119e30f5
NilsOveTen <nils.ove.tendenes@acando.no>
2020-03-31 08:42:28 +0200

    feat: add default prefixes to responses

--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/service/DatasetService.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/service/DatasetService.kt
@@ -2,6 +2,7 @@ package no.digdir.informasjonsforvaltning.fdk_dataset_harvester.service
 
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.fuseki.DatasetFuseki
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.JenaType
+import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.addDefaultPrefixes
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.createRDFResponse
 import org.springframework.stereotype.Service
 
@@ -11,11 +12,13 @@ class DatasetService(private val datasetFuseki: DatasetFuseki) {
     fun getAllDataServices(returnType: JenaType): String =
         datasetFuseki
             .fetchCompleteModel()
+            .addDefaultPrefixes()
             .createRDFResponse(returnType)
 
     fun getDataService(id: String, returnType: JenaType): String? =
         datasetFuseki
             .fetchByGraphName(id)
+            ?.addDefaultPrefixes()
             ?.createRDFResponse(returnType)
 
 }
