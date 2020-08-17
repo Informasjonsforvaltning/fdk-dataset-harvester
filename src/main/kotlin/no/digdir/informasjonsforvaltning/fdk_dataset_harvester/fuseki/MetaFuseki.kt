c80ac13d37f2b9940db562aa6fe4df69b4832271
Alexander Zakharov <alexander.zakharov@capgemini.com>
2020-08-17 09:41:11 +0200

    feat: #3155 add SPARQL endpoints for metadata

--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/fuseki/MetaFuseki.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/fuseki/MetaFuseki.kt
@@ -2,11 +2,14 @@ package no.digdir.informasjonsforvaltning.fdk_dataset_harvester.fuseki
 
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.configuration.FusekiProperties
 import org.apache.jena.query.ReadWrite
+import org.apache.jena.query.ResultSet
+import org.apache.jena.query.ResultSetFormatter
 import org.apache.jena.rdf.model.Model
 import org.apache.jena.rdfconnection.RDFConnection
 import org.apache.jena.rdfconnection.RDFConnectionFuseki
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
+import java.io.ByteArrayOutputStream
 
 private val LOGGER = LoggerFactory.getLogger(MetaFuseki::class.java)
 
@@ -47,6 +50,32 @@ class MetaFuseki(private val fusekiProperties: FusekiProperties) {
             }
         }
 
+    fun querySelect(query: String, format: QueryResponseFormat): String? =
+        metaConnection().use {
+            it.begin(ReadWrite.READ)
+            var result: String? = null
+            try {
+                it.queryResultSet(query) {
+                    qs ->
+                    result = when (format) {
+                        QueryResponseFormat.TEXT -> ResultSetFormatter.asText(qs).valuesOrNull(format)
+                        QueryResponseFormat.JSON -> querySelectWithJsonResponse(qs)?.valuesOrNull(format)
+                        QueryResponseFormat.XML -> ResultSetFormatter.asXMLString(qs).valuesOrNull(format)
+                    }
+                }
+            } catch (ex : Exception) {
+                LOGGER.error("sparql-select exception: $ex")
+                throw ex
+            }
+            return result
+        }
+
+    private fun querySelectWithJsonResponse(qs: ResultSet): String? {
+        val outputStream = ByteArrayOutputStream()
+        ResultSetFormatter.outputAsJSON(outputStream,qs)
+        return String(outputStream.toByteArray())
+    }
+
     fun saveWithGraphName(graphName: String, model: Model) =
         metaConnection().use {
             it.begin(ReadWrite.WRITE)
@@ -54,3 +83,15 @@ class MetaFuseki(private val fusekiProperties: FusekiProperties) {
         }
 
 }
+
+private fun String.valuesOrNull(format: QueryResponseFormat): String? =
+    when(format){
+        QueryResponseFormat.TEXT -> if (this.split('\n').size > 5) this else null
+        QueryResponseFormat.JSON -> if (this.split("\"bindings\": [")[1]
+                .replace(Regex("[^A-Za-z0-9]"), "")
+                .isNotEmpty()) this else null
+        QueryResponseFormat.XML -> if (this.split("<results>")[1]
+                .split("</results>")[0]
+                .replace(Regex("[^A-Za-z0-9]"), "")
+                .isNotEmpty()) this else null
+    }
--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/service/SparqlService.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/service/SparqlService.kt
@@ -1,18 +1,21 @@
 package no.digdir.informasjonsforvaltning.fdk_dataset_harvester.service
 
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.fuseki.HarvestFuseki
+import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.fuseki.MetaFuseki
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.fuseki.QueryResponseFormat
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.JenaType
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.addDefaultPrefixes
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rdf.createRDFResponse
-import org.apache.jena.sparql.engine.http.QueryExceptionHTTP
 import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Service
 
 private val LOGGER = LoggerFactory.getLogger(SparqlService::class.java)
 
 @Service
-class SparqlService (private val harvestFuseki: HarvestFuseki) {
+class SparqlService(
+    private val harvestFuseki: HarvestFuseki,
+    private val metaFuseki: MetaFuseki
+) {
 
     fun sparqlDescribe(query: String): String? =
         when {
@@ -31,8 +34,7 @@ class SparqlService (private val harvestFuseki: HarvestFuseki) {
     fun sparqlSelect(query: String, format: String): String? =
         when {
             query.toUpperCase().contains("SELECT") ->
-                    harvestFuseki.querySelect(query, format.getQueryResponseFormat()
-                    )
+                harvestFuseki.querySelect(query, format.getQueryResponseFormat())
             else -> {
                 LOGGER.info("select query does not contain select, will not be executed: $query")
                 throw IllegalArgumentException()
@@ -60,9 +62,35 @@ class SparqlService (private val harvestFuseki: HarvestFuseki) {
                 throw IllegalArgumentException()
             }
         }
+
+    fun sparqlMetaDescribe(query: String): String? =
+        when {
+            query.toUpperCase().contains("DESCRIBE") -> {
+                metaFuseki.queryDescribe(query)
+                    ?.addDefaultPrefixes()
+                    ?.createRDFResponse(JenaType.TURTLE)
+            }
+            else -> {
+                LOGGER.info("describe query does not contain describe, will not be executed: $query")
+                throw IllegalArgumentException()
+            }
+        }
+
+
+    fun sparqlMetaSelect(query: String, format: String): String? =
+        when {
+            query.toUpperCase().contains("SELECT") ->
+                metaFuseki.querySelect(query, format.getQueryResponseFormat()
+                )
+            else -> {
+                LOGGER.info("select query does not contain select, will not be executed: $query")
+                throw IllegalArgumentException()
+            }
+        }
+
 }
 
-private fun String.getQueryResponseFormat(): QueryResponseFormat = when(this) {
+private fun String.getQueryResponseFormat(): QueryResponseFormat = when (this) {
     "text/plain" -> QueryResponseFormat.TEXT
     "application/json" -> QueryResponseFormat.JSON
     "application/xml" -> QueryResponseFormat.XML
