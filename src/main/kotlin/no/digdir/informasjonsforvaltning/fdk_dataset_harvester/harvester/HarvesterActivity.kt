9716caef13767447847260d9115c17898c7e0450
NilsOveTen <nils.ove.tendenes@acando.no>
2020-09-06 09:36:22 +0200

    chore: rm coroutines

--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/harvester/HarvesterActivity.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/harvester/HarvesterActivity.kt
@@ -1,69 +1,50 @@
 package no.digdir.informasjonsforvaltning.fdk_dataset_harvester.harvester
 
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.cancelChildren
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.sync.Semaphore
-import kotlinx.coroutines.sync.withPermit
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.adapter.HarvestAdminAdapter
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.rabbit.RabbitMQPublisher
 import org.slf4j.LoggerFactory
-import org.springframework.stereotype.Service
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
 import org.springframework.util.MultiValueMap
-import java.util.*
-import javax.annotation.PostConstruct
+import java.util.Calendar
 
 private val LOGGER = LoggerFactory.getLogger(HarvesterActivity::class.java)
 private const val DATASET_TYPE = "dataset"
 private const val HARVEST_ALL_ID = "all"
 
-@Service
+@Component
+@Order(0)
 class HarvesterActivity(
     private val harvestAdminAdapter: HarvestAdminAdapter,
     private val harvester: DatasetHarvester,
     private val publisher: RabbitMQPublisher
-): CoroutineScope by CoroutineScope(Dispatchers.Default) {
+): ApplicationListener<ApplicationReadyEvent> {
 
-    private val activitySemaphore = Semaphore(1)
-    private val harvestSemaphore = Semaphore(2)
-
-    @PostConstruct
-    private fun fullHarvestOnStartup() = initiateHarvest(null)
+    override fun onApplicationEvent(event: ApplicationReadyEvent?) {
+        initiateHarvest(null)
+    }
 
     fun initiateHarvest(params: MultiValueMap<String, String>?) {
         if (params == null) LOGGER.debug("starting harvest of all datasets")
         else LOGGER.debug("starting harvest with parameters $params")
 
-        val harvest = launch {
-            activitySemaphore.withPermit {
-                harvestAdminAdapter.getDataSources(params)
-                    .filter { it.dataType == DATASET_TYPE }
-                    .forEach {
-                        if (it.url != null) {
-                            launch {
-                                harvestSemaphore.withPermit {
-                                    try {
-                                        harvester.harvestDatasetCatalog(it, Calendar.getInstance())
-                                    } catch (exception: Exception) {
-                                        LOGGER.error("Harvest of ${it.url} failed", exception)
-                                    }
-                                }
-                            }
-                        }
+        harvestAdminAdapter.getDataSources(params)
+            .filter { it.dataType == DATASET_TYPE }
+            .forEach {
+                if (it.url != null) {
+                    try {
+                        harvester.harvestDatasetCatalog(it, Calendar.getInstance())
+                    } catch (exception: Exception) {
+                        LOGGER.error("Harvest of ${it.url} failed", exception)
                     }
+                }
             }
-        }
 
-        val onHarvestCompletion = launch {
-            harvest.join()
-            LOGGER.debug("completed harvest with parameters $params")
+        LOGGER.debug("completed harvest with parameters $params")
 
-            publisher.send(HARVEST_ALL_ID)
-            harvest.cancelChildren()
-            harvest.cancel()
-        }
+        publisher.send(HARVEST_ALL_ID)
 
-        onHarvestCompletion.invokeOnCompletion { onHarvestCompletion.cancel() }
     }
 }
