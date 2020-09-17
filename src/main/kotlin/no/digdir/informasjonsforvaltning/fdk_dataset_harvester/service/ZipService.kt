f3cec9caf5f08d426843d8211c37595e4449da3d
NilsOveTen <github.nils.ove@tendenes.com>
2020-09-17 09:45:00 +0200

    chore: change base64 encoder

--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/service/ZipService.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/service/ZipService.kt
@@ -1,19 +1,19 @@
 package no.digdir.informasjonsforvaltning.fdk_dataset_harvester.service
 
 import java.io.ByteArrayOutputStream
-import java.util.*
 import java.util.zip.GZIPInputStream
 import java.util.zip.GZIPOutputStream
+import javax.xml.bind.DatatypeConverter
 import kotlin.text.Charsets.UTF_8
 
 fun gzip(content: String): String {
     val bos = ByteArrayOutputStream()
     GZIPOutputStream(bos).bufferedWriter(UTF_8).use { it.write(content) }
-    return Base64.getEncoder().encodeToString(bos.toByteArray())
+    return DatatypeConverter.printBase64Binary(bos.toByteArray())
 }
 
 fun ungzip(base64Content: String): String {
-    val content = Base64.getDecoder().decode(base64Content)
+    val content = DatatypeConverter.parseBase64Binary(base64Content)
     return GZIPInputStream(content.inputStream())
         .bufferedReader(UTF_8)
         .use { it.readText() }
