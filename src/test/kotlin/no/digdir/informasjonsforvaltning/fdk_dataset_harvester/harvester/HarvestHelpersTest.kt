4629d4d19c3444112745c88ac47dfd88bb5fd3dd
NilsOveTen <github.nils.ove@tendenes.com>
2022-06-21 09:08:44 +0200

    chore: add test of blank node skolemization

--- src/test/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/harvester/HarvestHelpersTest.kt
+++ src/test/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/harvester/HarvestHelpersTest.kt
@@ -1,10 +1,15 @@
 package no.digdir.informasjonsforvaltning.fdk_dataset_harvester.harvester
 
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.utils.TestResponseReader
+import org.apache.jena.sparql.vocabulary.FOAF
 import org.apache.jena.vocabulary.DCAT
+import org.apache.jena.vocabulary.DCTerms
 import org.apache.jena.vocabulary.RDF
 import org.junit.jupiter.api.Tag
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
 
 
 @Tag("unit")
@@ -34,6 +39,33 @@ class HarvestHelpersTest {
         val expected = responseReader.parseFile("parsed_dataset_1.ttl", "TURTLE")
 
         assert(dataset.harvestedDataset.isIsomorphicWith(expected))
+    }
+
+    @Test
+    fun blankNodesAreSkolemized() {
+        val harvested = responseReader.parseFile("catalog_with_blank_node.ttl", "TURTLE")
+
+        val extracted = extractCatalogs(harvested, "https://example.com")
+        assertEquals(1, extracted.size)
+
+        val catalog = extracted.first()
+        assertFalse(catalog.harvestedCatalog.isIsomorphicWith(harvested))
+
+        val agents = catalog.harvestedCatalog.listResourcesWithProperty(RDF.type, FOAF.Agent).toList()
+        assertEquals(1, agents.size)
+        val blankAgent = agents.first()
+        assertTrue(blankAgent.isURIResource)
+        assertTrue(blankAgent.uri.contains("https://testdirektoratet.no/model/blank-catalog/0"))
+        assertTrue(blankAgent.hasLiteral(DCTerms.identifier, "123456789"))
+        assertTrue(blankAgent.hasLiteral(FOAF.name, "Blanke"))
+
+        val periods = catalog.harvestedCatalog.listResourcesWithProperty(RDF.type, DCTerms.PeriodOfTime).toList()
+        assertEquals(1, periods.size)
+        val blankPeriod = periods.first()
+        assertTrue(blankPeriod.isURIResource)
+        assertTrue(blankPeriod.uri.contains("https://testdirektoratet.no/model/blank-dataset/0"))
+        assertTrue(blankPeriod.hasLiteral(DCAT.startDate, "2019-04-02T00:00:00"))
+        assertTrue(blankPeriod.hasLiteral(DCAT.endDate, "2022-06-06T00:00:00"))
     }
 
 }
