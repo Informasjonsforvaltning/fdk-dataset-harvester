15e0384585698a0a7ccda4aeaf5a5bdbe3402ebf
NilsOveTen <github.nils.ove@tendenes.com>
2022-06-22 13:24:27 +0200

    chore: add forceUpdate to incoming rabbit messages

--- src/test/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/service/HarvestAdminAdapter.kt
+++ src/test/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/service/HarvestAdminAdapter.kt
@@ -44,7 +44,7 @@ class HarvestAdminAdapterTest {
             whenever(valuesMock.harvestAdminRootUrl)
                 .thenReturn("http://www.example.com")
 
-            val url = adapter.urlWithParameters(HarvestAdminParameters())
+            val url = adapter.urlWithParameters(HarvestAdminParameters(null, null, null))
 
             assertEquals(URL("http://www.example.com/datasources?dataType=dataset"), url)
         }
@@ -58,7 +58,7 @@ class HarvestAdminAdapterTest {
                 HarvestAdminParameters(
                     publisherId = "123456789",
                     dataSourceType = "DCAT-AP-NO",
-                    dataType = "dataset"
+                    dataSourceId = null
                 )
             )
 
