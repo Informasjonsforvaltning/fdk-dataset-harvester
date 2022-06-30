547e25640bc04ee223ec6dcace415a88e9e42d4d
NilsOveTen <github.nils.ove@tendenes.com>
2022-06-30 09:11:26 +0200

    chore: replace deprecated security config

--- src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/Application.kt
+++ src/main/kotlin/no/digdir/informasjonsforvaltning/fdk_dataset_harvester/Application.kt
@@ -4,9 +4,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import no.digdir.informasjonsforvaltning.fdk_dataset_harvester.configuration.ApplicationProperties
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties
 
 @SpringBootApplication
-@EnableConfigurationProperties(ApplicationProperties::class)
+@EnableConfigurationProperties(ApplicationProperties::class, OAuth2ResourceServerProperties::class)
 open class Application
 
 fun main(args: Array<String>) {
